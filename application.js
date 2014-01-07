621c57be4a7d086c678ae54aeaeddaa74233797f
Robert Wheeler <3robert.wheeler3@gmail.com>
2014-01-07 09:31:04 -0800

    Initial Upload
    Initial upload of 37 Signals Revisited site to GitHub

---
+++ application.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+	
+	$(".greeting").html(function() {
+			var today = (["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][(new Date()).getDay()]);
+			$(this).html("Happy " + today + ".");
+		})
+	
+	$('#basecamp').hover(function() {
+		$('#headers').toggleClass('invisible');
+		});
+	$('#basecamp').hover(function() {
+		$('#basecampheader').toggleClass('invisible');
+		});
+	$('#basecamp').mouseenter(function() {
+		$('.basecamparrow').show();
+		});
+	$('#basecamp').mouseleave(function() {
+		$('.basecamparrow').hide();
+		});
+		
+	$('#highrise').hover(function() {
+		$('#headers').toggleClass('invisible');
+		});
+	$('#highrise').hover(function() {
+		$('#highriseheader').toggleClass('invisible');
+		});
+	$('#highrise').mouseenter(function() {
+		$('.highrisearrow').show();
+		});
+	$('#highrise').mouseleave(function() {
+		$('.highrisearrow').hide();
+		});
+		
+	$('#campfire').hover(function() {
+		$('#headers').toggleClass('invisible');
+		});
+	$('#campfire').hover(function() {
+		$('#campfireheader').toggleClass('invisible');
+		});
+	$('#campfire').mouseenter(function() {
+		$('.campfirearrow').show();
+		});
+	$('#campfire').mouseleave(function() {
+		$('.campfirearrow').hide();
+		});
+		
+});
